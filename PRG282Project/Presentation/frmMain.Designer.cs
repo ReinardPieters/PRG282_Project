5362b8503146b5e979eba7c3d3f107349a91f701
Estian Stander <143404203+EstianStander@users.noreply.github.com>
2024-10-30 15:48:36 +0200

    new form

--- PRG282Project/Presentation/frmMain.Designer.cs
+++ PRG282Project/Presentation/frmMain.Designer.cs
@@ -34,50 +34,56 @@
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(10, 10);
+            this.btnLogout.Location = new System.Drawing.Point(13, 12);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(75, 23);
+            this.btnLogout.Size = new System.Drawing.Size(100, 28);
             this.btnLogout.TabIndex = 0;
             this.btnLogout.Text = "Log Out";
             this.btnLogout.UseVisualStyleBackColor = true;
             // 
             // txtStudentID
             // 
-            this.txtStudentID.Location = new System.Drawing.Point(20, 50);
+            this.txtStudentID.Location = new System.Drawing.Point(27, 62);
+            this.txtStudentID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtStudentID.Name = "txtStudentID";
-            this.txtStudentID.Size = new System.Drawing.Size(150, 20);
+            this.txtStudentID.Size = new System.Drawing.Size(282, 22);
             this.txtStudentID.TabIndex = 1;
             this.txtStudentID.Text = "Enter Student ID";
             // 
             // txtStudentName
             // 
-            this.txtStudentName.Location = new System.Drawing.Point(20, 80);
+            this.txtStudentName.Location = new System.Drawing.Point(27, 98);
+            this.txtStudentName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtStudentName.Name = "txtStudentName";
-            this.txtStudentName.Size = new System.Drawing.Size(150, 20);
+            this.txtStudentName.Size = new System.Drawing.Size(282, 22);
             this.txtStudentName.TabIndex = 2;
             this.txtStudentName.Text = "Enter Student Name";
             // 
             // txtAge
             // 
-            this.txtAge.Location = new System.Drawing.Point(20, 110);
+            this.txtAge.Location = new System.Drawing.Point(400, 62);
+            this.txtAge.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtAge.Name = "txtAge";
-            this.txtAge.Size = new System.Drawing.Size(150, 20);
+            this.txtAge.Size = new System.Drawing.Size(294, 22);
             this.txtAge.TabIndex = 3;
             this.txtAge.Text = "Enter Age";
             // 
             // txtCourse
             // 
-            this.txtCourse.Location = new System.Drawing.Point(20, 140);
+            this.txtCourse.Location = new System.Drawing.Point(400, 98);
+            this.txtCourse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCourse.Name = "txtCourse";
-            this.txtCourse.Size = new System.Drawing.Size(150, 20);
+            this.txtCourse.Size = new System.Drawing.Size(294, 22);
             this.txtCourse.TabIndex = 4;
             this.txtCourse.Text = "Enter Course";
             // 
             // btnViewAll
             // 
-            this.btnViewAll.Location = new System.Drawing.Point(200, 50);
+            this.btnViewAll.Location = new System.Drawing.Point(246, 172);
+            this.btnViewAll.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnViewAll.Name = "btnViewAll";
-            this.btnViewAll.Size = new System.Drawing.Size(100, 23);
+            this.btnViewAll.Size = new System.Drawing.Size(133, 28);
             this.btnViewAll.TabIndex = 5;
             this.btnViewAll.Text = "View All Students";
             this.btnViewAll.UseVisualStyleBackColor = true;
@@ -85,27 +91,31 @@
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(200, 80);
+            this.btnUpdate.Location = new System.Drawing.Point(400, 172);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(100, 23);
+            this.btnUpdate.Size = new System.Drawing.Size(133, 28);
             this.btnUpdate.TabIndex = 6;
             this.btnUpdate.Text = "Update Student";
             this.btnUpdate.UseVisualStyleBackColor = true;
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(200, 110);
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(639, 172);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(100, 23);
+            this.btnDelete.Size = new System.Drawing.Size(133, 28);
             this.btnDelete.TabIndex = 7;
             this.btnDelete.Text = "Delete Student";
-            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.UseVisualStyleBackColor = false;
             // 
             // btnGenerateReport
             // 
-            this.btnGenerateReport.Location = new System.Drawing.Point(200, 140);
+            this.btnGenerateReport.Location = new System.Drawing.Point(27, 490);
+            this.btnGenerateReport.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGenerateReport.Name = "btnGenerateReport";
-            this.btnGenerateReport.Size = new System.Drawing.Size(100, 23);
+            this.btnGenerateReport.Size = new System.Drawing.Size(133, 28);
             this.btnGenerateReport.TabIndex = 8;
             this.btnGenerateReport.Text = "Generate Report";
             this.btnGenerateReport.UseVisualStyleBackColor = true;
@@ -113,34 +123,38 @@
             // dgvStudents
             // 
             this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvStudents.Location = new System.Drawing.Point(20, 180);
+            this.dgvStudents.Location = new System.Drawing.Point(27, 222);
+            this.dgvStudents.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvStudents.Name = "dgvStudents";
-            this.dgvStudents.Size = new System.Drawing.Size(500, 200);
+            this.dgvStudents.RowHeadersWidth = 51;
+            this.dgvStudents.Size = new System.Drawing.Size(745, 246);
             this.dgvStudents.TabIndex = 9;
             // 
             // lblTotalStudents
             // 
             this.lblTotalStudents.AutoSize = true;
-            this.lblTotalStudents.Location = new System.Drawing.Point(20, 400);
+            this.lblTotalStudents.Location = new System.Drawing.Point(427, 531);
+            this.lblTotalStudents.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTotalStudents.Name = "lblTotalStudents";
-            this.lblTotalStudents.Size = new System.Drawing.Size(88, 13);
+            this.lblTotalStudents.Size = new System.Drawing.Size(106, 16);
             this.lblTotalStudents.TabIndex = 10;
             this.lblTotalStudents.Text = "Total Students: 0";
             // 
             // lblAverageAge
             // 
             this.lblAverageAge.AutoSize = true;
-            this.lblAverageAge.Location = new System.Drawing.Point(20, 420);
+            this.lblAverageAge.Location = new System.Drawing.Point(170, 531);
+            this.lblAverageAge.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblAverageAge.Name = "lblAverageAge";
-            this.lblAverageAge.Size = new System.Drawing.Size(81, 13);
+            this.lblAverageAge.Size = new System.Drawing.Size(100, 16);
             this.lblAverageAge.TabIndex = 11;
             this.lblAverageAge.Text = "Average Age: 0";
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 500);
+            this.ClientSize = new System.Drawing.Size(800, 615);
             this.Controls.Add(this.btnLogout);
             this.Controls.Add(this.txtStudentID);
             this.Controls.Add(this.txtStudentName);
@@ -153,6 +167,7 @@
             this.Controls.Add(this.dgvStudents);
             this.Controls.Add(this.lblTotalStudents);
             this.Controls.Add(this.lblAverageAge);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMain";
             this.Text = "Student Management System";
             ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();

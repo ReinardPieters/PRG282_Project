6452562739641a5ecf708e660f73f7402e4e5584
Ec0_sEEME <estian.stander.git@gmail.com>
2024-11-03 22:53:32 +0200

    new color

--- PRG282Project/Presentation/frmMain.Designer.cs
+++ PRG282Project/Presentation/frmMain.Designer.cs
@@ -249,6 +249,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(817, 588);
             this.Controls.Add(this.lblCurrentUser);
             this.Controls.Add(this.groupBox2);
